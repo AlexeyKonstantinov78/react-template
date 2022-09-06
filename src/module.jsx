191b84c693dd396df27f5391963edd00f516ba53
AlexeyKonstantinov78 <alex_kav@mail.ru>
2022-09-06 11:10:29 +0300

    add react-template

---
+++ src/module.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import disk from './img/discord.png';
+
+const Module = () => (
+  <>
+    <h1>Hello, React!</h1>
+    <img src={disk} width="500" height="300" />
+  </>
+);
+
+export default Module;
